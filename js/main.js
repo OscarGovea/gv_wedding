a1253ccb512ab9c527c3973295c85c8eac933c1d
Govea García Oscar Daniel <ogovea@aseg.gob.mx>
2019-09-03 11:34:03 -0500

    ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+
+    //Download Invitacion
+    $("#btnDownloadInvitacion").click(function(){
+        var url = "" + "/img/"
+        window.open(url, "_blank");
+    });
+
+
 });
+
