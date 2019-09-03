27dfef32655c60bd7b12e54e70ea1df713ac9456
Govea García Oscar Daniel <ogovea@aseg.gob.mx>
2019-09-03 15:35:03 -0500

    add swal, and invitados validate

--- js/main.js
+++ js/main.js
@@ -1,10 +1,60 @@
 $(document).ready(function(){
 
+    //Invitados
+    var lstInvitados = 
+    [
+       {
+        nombre: "Oscar Govea",
+        password: "123",
+        noInvitados: 2,
+        mesa: 12
+       },
+       {
+        nombre: "Lucia Vargas",
+        password: "123",
+        noInvitados: 3,
+        mesa: 13
+       }  
+    ];
+    
     //Download Invitacion
     $("#btnDownloadInvitacion").click(function(){
-        var url = "" + "/img/"
-        window.open(url, "_blank");
+
+        var user = $("#txtUser").val();
+        var pass = $("#txtPassword").val();
+        var qrcode;
+
+        for(var element=0; element<lstInvitados.length; element++){
+            if(user === lstInvitados[element].nombre && pass === lstInvitados[element].password){
+                $("#qrDiv").html("<div id='qrcode'></div>");
+                qrcode = new QRCode("qrcode", {
+                    text: "Boda O & L\n" + 
+                          "Nombre: "+ lstInvitados[element].nombre + "\n" +
+                          "No. Invitados: " + lstInvitados[element].noInvitados + "\n"+
+                          "Mesa: "+ lstInvitados[element].mesa + "\n",
+                    width: 210,
+                    height: 210,
+                    colorDark : "#000000",
+                    colorLight : "#ffffff",
+                    correctLevel : QRCode.CorrectLevel.H
+                });
+                return true;
+            }
+            else{
+                Swal.fire(
+                    'Intenta Nuevamente!',
+                    'Probablemente no estes invitado',
+                    'error'
+                  )
+                $("#txtUser").val("");
+                $("#txtPassword").val("");
+                $("#qrDiv").html("");
+                return true;
+            }
+        }
     });
+
+
 
 
 });
