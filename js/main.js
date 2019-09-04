475d50fd6f7f3fb8e4206b6ec8908f0a0a7a82c1
Govea García Oscar Daniel <ogovea@aseg.gob.mx>
2019-09-04 13:25:50 -0500

    upload images, finish user and pass validation, add some styles

--- js/main.js
+++ js/main.js
@@ -1,61 +1,72 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    $("#mensajeQR").hide();
     //Invitados
-    var lstInvitados = 
-    [
-       {
-        nombre: "Oscar Govea",
-        password: "123",
-        noInvitados: 2,
-        mesa: 12
-       },
-       {
-        nombre: "Lucia Vargas",
-        password: "123",
-        noInvitados: 3,
-        mesa: 13
-       }  
-    ];
-    
+    var lstInvitados =
+        [
+            {
+                nombre: "Admin",
+                password: "123",
+                noInvitados: 2,
+                mesa: 12
+            },
+            {
+                nombre: "Oscar Govea",
+                password: "123",
+                noInvitados: 2,
+                mesa: 12
+            },
+            {
+                nombre: "Lucia Vargas",
+                password: "123",
+                noInvitados: 3,
+                mesa: 13
+            }
+        ];
+
+    lstInvitados.length;
+
     //Download Invitacion
-    $("#btnDownloadInvitacion").click(function(){
+    $("#btnDownloadInvitacion").click(function () {
 
         var user = $("#txtUser").val();
         var pass = $("#txtPassword").val();
         var qrcode;
+        var encontrado;
 
-        for(var element=0; element<lstInvitados.length; element++){
-            if(user === lstInvitados[element].nombre && pass === lstInvitados[element].password){
-                $("#qrDiv").html("<div id='qrcode'></div>");
-                qrcode = new QRCode("qrcode", {
-                    text: "Boda O & L\n" + 
-                          "Nombre: "+ lstInvitados[element].nombre + "\n" +
-                          "No. Invitados: " + lstInvitados[element].noInvitados + "\n"+
-                          "Mesa: "+ lstInvitados[element].mesa + "\n",
-                    width: 210,
-                    height: 210,
-                    colorDark : "#000000",
-                    colorLight : "#ffffff",
-                    correctLevel : QRCode.CorrectLevel.H
-                });
-                return true;
-            }
-            else{
-                Swal.fire(
-                    'Intenta Nuevamente!',
-                    'Probablemente no estes invitado',
-                    'error'
-                  )
-                $("#txtUser").val("");
-                $("#txtPassword").val("");
-                $("#qrDiv").html("");
-                return true;
+        for (var element = 0; element < lstInvitados.length; element++) {
+            encontrado = false;
+            if (user === lstInvitados[element].nombre && pass === lstInvitados[element].password) {
+                encontrado = true;
+                break;
             }
         }
+        if (encontrado) {
+            $("#qrDiv").html("<div id='qrcode'></div>");
+            qrcode = new QRCode("qrcode", {
+                text: "Boda O & L\n" +
+                    "Nombre: " + lstInvitados[element].nombre + "\n" +
+                    "No. Invitados: " + lstInvitados[element].noInvitados + "\n" +
+                    "Mesa: " + lstInvitados[element].mesa + "\n",
+                width: 210,
+                height: 210,
+                colorDark: "#000000",
+                colorLight: "#ffffff",
+                correctLevel: QRCode.CorrectLevel.H
+            });
+            $("#nombreInvitado").html(lstInvitados[element].nombre);
+            $("#mensajeQR").show();
+        }
+        else {
+            Swal.fire(
+                'Intenta Nuevamente!',
+                'Probablemente no estes invitado',
+                'error'
+            )
+            $("#mensajeQR").hide();
+            $("#txtUser").val("");
+            $("#txtPassword").val("");
+            $("#qrDiv").html("");
+        }
     });
-
-
-
-
 });
 
